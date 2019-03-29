bf1da550c05e1ca69e09623c7a8a7833eedde376
chiaki-kato <makuragi@me.com>
2019-03-29 22:41:45 +0900

    completed.

--- high_low.py
+++ high_low.py
@@ -1,67 +1,90 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from enum import auto, Enum, IntEnum
 import random
-import readchar
-import sys
+
+
+class Card:
+    SUITS = '♡♢♤♧'
+    RANKS = '0 2 3 4 5 6 7 8 9 10 J Q K A'.split()
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __repr__(self):
+        return f'{Card.SUITS[self.suit]} {Card.RANKS[self.rank]}'
 
 
 class Deck:
-    deck = []
-
     def __init__(self):
-        suits = ['♡', '♢', '♤', '♧']
-        ranks = range(2, 15)
-        self.deck = [(i, j) for i in suits for j in ranks]
-        random.shuffle(self.deck)
+        self.cards = [Card(suit, rank) for suit in range(4)
+                      for rank in range(1, 14)]
+        random.shuffle(self.cards)
 
+    @property
     def draw(self):
-        return self.deck.pop()
+        return self.cards.pop()
 
 
 def ask():
-    answer = input('say, high or low: ')
+    answer = input('say, high or low:')
     return answer
 
 
-def validate(answer, selection):
-    if answer in selection:
+def validate(answer, has_answer):
+    if answer in has_answer:
         return True
     else:
         return False
+
+
+def select(is_valid, answer):
+    if is_valid:
+        if answer == 'high':
+            return True
+        else:
+            return False
 
 
 def is_high(player_hand, dealer_hand):
-    if player_hand[1] - dealer_hand[1] > 0:
+    is_high = player_hand.rank - dealer_hand.rank
+    if is_high > 0:
         return True
     else:
         return False
+
+
+def result(high, selection):
+    if is_high == 0:
+        print('you lose.')
+    elif high == selection:
+        print('you win!')
+    else:
+        print('you lose.')
 
 
 def main():
     deck = Deck()
-    player_hand = deck.draw()
-    dealer_hand = deck.draw()
-    selection = ['high', 'low']
+    player_hand = deck.draw
+    dealer_hand = deck.draw
+    has_answer = ['high', 'low']
 
-    print('dealer hand :', dealer_hand)
+    print('dealer hand:', dealer_hand)
 
     answer = ask()
-    is_valid = validate(answer, selection)
+    is_valid = validate(answer, has_answer)
 
     if not is_valid:
         print('no valid input. plz input high or low.')
         return
 
-    print('your hand :', player_hand)
-
+    selection = select(is_valid, answer)
     high = is_high(player_hand, dealer_hand)
 
-    if (high and answer == 'high') or (not high and answer == 'low'):
-        print('you win!')
-    else:
-        print('you lose.')
+    print('your hand:', player_hand)
+
+    result(high, selection)
 
 
 if __name__ == '__main__':
