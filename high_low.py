40f7af4ad210f84b3cfd7bc17c621307d1546c4b
chiaki-kato <makuragi@me.com>
2019-04-13 02:29:23 +0900

    improve handling exceptions

--- high_low.py
+++ high_low.py
@@ -6,7 +6,7 @@ import sys
 
 
 class Card:
-    SUITS = '? ? ? ?'
+    SUITS = '♡♢♤♧'
     RANKS = '0 2 3 4 5 6 7 8 9 10 J Q K A'.split()
 
     def __init__(self, suit, rank):
@@ -28,18 +28,32 @@ class Deck:
         return self.cards.pop()
 
 
+class MyError(Exception):
+    pass
+
+
+class InputError(MyError):
+    pass
+
+ # ??????????????
+
+
 def ask():
-    error_message = 'invalid input. please enter 0 or 1 as a number.'
+
+    def validate(answer):
+        x = [0, 1]
+        if not answer in x:
+            raise InputError(answer)
+        return answer
+
     try:
         answer = int(input('press the 1 or 0 key. high(1) / low(0): '))
+        validate(answer)
         return answer
-    except (ValueError, TypeError):
-        print(error_message)
+
+    except (InputError, ValueError, TypeError):
+        print('invalid input. please enter 0 or 1 as a number.')
         exit()
-    else:
-        if not answer is 0 or 1:
-            print(error_message)
-            exit()
 
 
 def is_high(player_hand, dealer_hand):
@@ -65,7 +79,7 @@ def main():
     dealer_hand = deck.draw
 
     print('dealer hand:', dealer_hand)
-
+    # ask()
     answer = ask()
     high = is_high(player_hand, dealer_hand)
 
